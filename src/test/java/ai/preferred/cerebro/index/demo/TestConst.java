5633b1b63fe65585b184b890738d32af980028a4
AndrewDungLe <andrew.dung.le@gmail.com>
2019-06-15 10:38:00 +0800

    remove cerebro.index.demo

--- src/test/java/ai/preferred/cerebro/index/demo/TestConst.java
+++
@@ -1,8 +0,0 @@
-package ai.preferred.cerebro.index.demo;
-
-public class TestConst {
-    public final static String DIM_50_PATH = "C:\\50_dims_testing\\";
-    public final static String TXT_DATA_PATH = "E:\\Lucene\\imdb_data\\";
-    public final static String DIM_100_PATH = "E:\\100_dims_testing\\";
-    public static final String FilePathField = "path_to_file";
-}
