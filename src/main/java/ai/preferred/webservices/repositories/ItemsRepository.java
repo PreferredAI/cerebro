25531b2f31989d206606e7fd3b6620f745b12e5d
mhuynhphu <hpminh@apcs.vn>
2019-09-11 10:35:13 +0800

    Revert "Added spring-boot webservices used in techfest"
    This reverts commit afc6afc174cc4f5d503bd1a950d66a90824904d8.

--- src/main/java/ai/preferred/webservices/repositories/ItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package ai.preferred.webservices.repositories;
-
-import ai.preferred.webservices.models.Items;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ItemsRepository extends MongoRepository<Items, ObjectId> {
-    Items findBy_id(ObjectId _id);
-}
--- src/main/java/ai/preferred/webservices/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package ai.preferred.webservices.repositories;
-
-import ai.preferred.webservices.models.Users;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UsersRepository extends MongoRepository<Users, ObjectId> {
-    Users findBy_id(ObjectId _id);
-}
