6191ebbcd22e96ae705c5b0ae07e15bf4376c0ee
Ulfbert <hpminh@apcs.vn>
2019-10-10 14:21:42 +0800

    fix test bugs

--- src/test/java/ai/preferred/cerebro/index/builder/PersonalizedDocFactoryTest.java
+++ src/test/java/ai/preferred/cerebro/index/builder/PersonalizedDocFactoryTest.java
@@ -1,5 +1,7 @@
 package ai.preferred.cerebro.index.builder;
 
+import ai.preferred.cerebro.common.IntID;
+import ai.preferred.cerebro.common.StringID;
 import ai.preferred.cerebro.index.demo.TestConst;
 import ai.preferred.cerebro.index.exception.DocNotClearedException;
 import ai.preferred.cerebro.index.exception.SameNameException;
@@ -27,7 +29,7 @@ class PersonalizedDocFactoryTest {
     @Test
     void createPersonalizedDoc(){
         try {
-            docFactory.createPersonalizedDoc("A01", TestConst.vec1);
+            docFactory.createPersonalizedDoc(new StringID("A01"), TestConst.vec1);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -43,7 +45,7 @@ class PersonalizedDocFactoryTest {
     @Test
     void createPersonalizedDocIntID(){
         try {
-            docFactory.createPersonalizedDoc(1, TestConst.vec1);
+            docFactory.createPersonalizedDoc(new IntID(1), TestConst.vec1);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,7 +54,7 @@ class PersonalizedDocFactoryTest {
     void createTextDoc() {
         TextField content = new TextField(IndexConst.CONTENTS, TestConst.text2, Field.Store.NO);
         try {
-            docFactory.createTextDoc("rwer", content);
+            docFactory.createTextDoc(new StringID("rwer"), content);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,7 +64,7 @@ class PersonalizedDocFactoryTest {
     void createTextDocIntID() {
         TextField content = new TextField(IndexConst.CONTENTS, TestConst.text2, Field.Store.NO);
         try {
-            docFactory.createTextDoc(23, content);
+            docFactory.createTextDoc(new IntID(23), content);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -70,38 +72,32 @@ class PersonalizedDocFactoryTest {
     @Test
     void testDocNotCleared() {
         Assertions.assertThrows(DocNotClearedException.class, ()->{
-            docFactory.createPersonalizedDoc("A01", TestConst.vec1);
-            docFactory.createPersonalizedDoc("A01", TestConst.vec2);
+            docFactory.createPersonalizedDoc(new StringID("A01"), TestConst.vec1);
+            docFactory.createPersonalizedDoc(new StringID("A01"), TestConst.vec2);
         });
     }
     
     @Test
     void testSameName() {
         Assertions.assertThrows(SameNameException.class, ()->{
-            docFactory.createPersonalizedDoc("A01", TestConst.vec1);
+            docFactory.createPersonalizedDoc(new StringID("A01"), TestConst.vec1);
             TextField content = new TextField(IndexConst.VecFieldName, TestConst.text1, Field.Store.NO);
             docFactory.addField(content);
         });
         Assertions.assertThrows(DocNotClearedException.class, ()->{
             TextField content = new TextField(IndexConst.CONTENTS, TestConst.text2, Field.Store.NO);
-            docFactory.createTextDoc("A01", content);
+            docFactory.createTextDoc(new StringID("A01"), content);
             TextField fieldwitherrorname = new TextField(IndexConst.VecFieldName, TestConst.text1, Field.Store.NO);
             docFactory.addField(fieldwitherrorname);
         });
     }
 
-    @Test
-    void testUnsupportedDataType(){
-        Assertions.assertThrows(UnsupportedDataType.class, ()->{
-            docFactory.createPersonalizedDoc(2.3, TestConst.vec1);
-        });
-    }
 
     @Test
     void testHashingVecNotProvided(){
         Assertions.assertThrows(Exception.class, ()->{
             PersonalizedDocFactory docFactory1 = new PersonalizedDocFactory();
-            docFactory1.createPersonalizedDoc(2, TestConst.vec1);
+            docFactory1.createPersonalizedDoc(new IntID(2), TestConst.vec1);
         });
     }
 
